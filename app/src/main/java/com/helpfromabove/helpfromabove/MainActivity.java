87433a76f413f19443b22160e5b8477c2c978cfe
Caleb Smith <caleb.smith.2015@gmail.com>
2017-05-08 20:42:45 -0400

    Added OnClick calls and fixed button layout.
    Buttons from content_main now call methods in MainActivity when pressed.
    These then call methods that will be implemented to actually send the
    command to the UASC.
    Before, the Emergency button was much wider than the other commands. A
    minimum width has been added to the command buttons/switches. This helps
    make all of the commands equal in width. This, however, causes issues on
    screens with lower resolution. command_grid_layout has also
    been centered horizontally to help center all of the burrons.
    The light button has been changed to a switch to help the user tell if
    the light is on or off.
    Basic debug logging has also been added to help identify when buttons
    are pressed, and what the new value of the light switch when it's
    pressed.

--- app/src/main/java/com/helpfromabove/helpfromabove/MainActivity.java
+++ app/src/main/java/com/helpfromabove/helpfromabove/MainActivity.java
@@ -1,15 +1,23 @@
 package com.helpfromabove.helpfromabove;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Switch;
+
+/**
+ * Created by Caleb Smithcs on 5/4/2017.
+ */
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +44,68 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.d(TAG, "onOptionsItemSelected: action_settings");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void emergencyButtonOnClick(View view) {
+        Log.d(TAG, "emergencyButtonOnClick: ");
+        emergency();
+    }
+
+    public void lightSwitchOnClick(View view) {
+        Log.d(TAG, "lightSwitchOnClick: ");
+        boolean isChecked = ((Switch) view).isChecked();
+        light(isChecked);
+    }
+
+    public void uasHeightUpButtonOnClick(View view) {
+        Log.d(TAG, "uasHeightUpButtonOnClick: ");
+        uasHeightUp();
+    }
+
+    public void uasHeightDownOnClick(View view) {
+        Log.d(TAG, "uasHeightDownOnClick: ");
+        uasHeightDown();
+    }
+    
+    public void sessionStartButtonOnCLick(View view) {
+        Log.d(TAG, "sessionStartButtonOnCLick: ");
+        sessionStart();
+    }
+
+
+    public void sessionEndButtonOnCLick(View view) {
+        Log.d(TAG, "sessionEndButtonOnCLick: ");
+        sessionEnd();
+    }
+    
+    private void emergency() {
+        Log.d(TAG, "emergency: ");
+    }
+
+
+    private void light(boolean isChecked) {
+        Log.d(TAG, "light: isChecked=" + isChecked);
+    }
+
+
+    private void uasHeightUp() {
+        Log.d(TAG, "uasHeightUp: ");
+    }
+    
+    private void uasHeightDown() {
+        Log.d(TAG, "uasHeightDown: ");
+    }
+    
+    private void sessionStart() {
+        Log.d(TAG, "sessionStart: ");
+    }
+    
+    private void sessionEnd() {
+        Log.d(TAG, "sessionEnd: ");
     }
 }
